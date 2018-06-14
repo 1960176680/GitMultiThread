31cc5cb96aec89a9e1f9acd45d0a45a69fd4c757
Sun <1960176680@qq.com>
2018-06-14 16:35:40 +0800

    多线程使用初始提交
    Signed-off-by: Sun <1960176680@qq.com>

---
+++ app/src/test/java/com/zhouwenguang/hz/myapplication/ExampleUnitTest.java
@@ -0,0 +1,25 @@
+package com.zhouwenguang.hz.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getNum() {
+        int DEFAULT_INITIAL_CAPACITY = 3 << 4;
+        System.out.print(DEFAULT_INITIAL_CAPACITY);
+
+    }
+
+}
